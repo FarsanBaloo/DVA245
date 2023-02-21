86547ee9b4ece1d7be71b03af7bb423c92a1efaa
FarsanBaloo <106886894+FarsanBaloo@users.noreply.github.com>
2023-02-21 14:43:52 +0100

    Working solution

--- Assignments/4-merge-sort-queue-begin/merge_queue_begin.py
+++ Assignments/4-merge-sort-queue-begin/merge_queue_begin.py
@@ -11,6 +11,16 @@ def merge(S1, S2):
        Queues are deques used with the front to the left."""
     S = deque()
     # TODO add the functionality that merges S1 and S2 into S
+    S = deque()
+    while S1 and S2:
+        if S1[0] <= S2[0]:
+            S.append(S1.popleft())
+        else:
+            S.append(S2.popleft())
+    while S1:
+        S.append(S1.popleft())
+    while S2:
+        S.append(S2.popleft())
     return S
   
 def merge_level_queues(level_queues):
@@ -27,8 +37,26 @@ def merge_sort(S):
     """Sort the elements of queue S using the merge-sort algorithm.
     The sorted result is returned in a new queue, S is left empty
     Queues are deques used with the front to the left. """
+    # TODO: 1. Create the level queue.
     level_queues = deque()
+
     # TODO: Create a queue for each input element and add them to the level_queues
-    # TODO: while we have more than one queue remaining, merge a level 
+    for element in S:
+        level_queues.append(deque([element]))
+
+    # TODO: while we have more than one queue remaining, merge a level
+    while len(level_queues) >1:
+        level_queues = merge_level_queues(level_queues)
+
     # TODO: dequeue and return the single remaining merged queue
+    if len(level_queues) == 1:
+        level_queues = level_queues.popleft()
+    return level_queues
+
+
+
+
+
+
+
 
